88933fbe7a5b324bac7d9019f6fb521dd71170b5
kr1pt0n <39812156+kr1pt0n@users.noreply.github.com>
2021-11-09 03:04:49 -0500

    Update rtk892.sh

--- rtk892.sh
+++ rtk892.sh
@@ -54,13 +54,13 @@ echo ""
 
 sleep 3
 
-echo -e "\n${yellowColour}	✓ [*] El modo de ahorro de energia sera deshabilitado"
+echo -e "\n${yellowColour}	✓ [*] La rutina de ahorro de energía del controlador de sonido sera deshabilitado"
 
 sleep 3
 
 echo "options snd-hda-intel probe_mask=1 model=auto power_save=0 power_save_controller=N" >> /etc/modprobe.d/alsa-base.conf
 
-echo -e "\n${purpleColour}	✓ [*] El modo de ahorro de energia y los pops en los sonidos fueron retirados de su sistema."
+echo -e "\n${purpleColour}	✓ [*] La rutina de ahorro de energía (pops),fue retirada de su sistema."
 
 sleep 3
 
